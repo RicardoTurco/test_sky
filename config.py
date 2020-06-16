ca441c12017e39330a92046b55a30d515da30b65
RicardoTurco <ricturco@gmail.com>
2020-06-16 00:19:12 -0300

    Add KEYS file to connect to Firebse DB

--- config.py
+++ config.py
@@ -1,9 +1,21 @@
+import sys
+from firebase_admin import credentials, firestore, initialize_app
+
+
 class BaseConfig:
 
     DEBUG = True
     RESTPLUS_VALIDATE = True
     ERROR_INCLUDE_MESSAGE = False
     RESTPLUS_MASK_SWAGGER = False
+
+    try:
+        SKY_KEY = credentials.Certificate('keys.json')
+        SKY_APP = initialize_app(SKY_KEY)
+        SKY_DB = firestore.client()
+    except Exception as e:
+        print(e)
+        sys.exit()
 
 
 class ProdConfig(BaseConfig):
